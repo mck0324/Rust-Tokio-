f8baccc6f157bae48fc1209060acb8dd8632f702
mck0324 <dch3030@naver.com>
2024-10-08 00:27:15 +0900

    sync

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use std::future;
 
-use tokio::time;
+use tokio::{io::AsyncReadExt, time};
 use log::Level;
 
-async fn run() {
+async fn sleeper() {
     log::info!("Sleeping");
     time::sleep(time::Duration::from_secs(1)).await;
     log::info!("Awake!");
 }
 
+async fn reader() {
+    log::info!("Reading some been data");
+    let mut f = tokio::fs::File::open("beeg.csv").await.unwrap();
+    let mut contents = vec![];
+    f.read_to_end(&mut contents).await.unwrap();
+    log::info!("Read beeg {}  bytes", contents.len());
+}
+
+async fn run() {
+    sleeper().await;
+    reader().await;
+}
 fn main() {
     simple_logger::init_with_level(Level::Info).unwrap();
 
